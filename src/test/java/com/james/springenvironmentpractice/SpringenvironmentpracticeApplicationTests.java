fcf3640afa2b4487e9026d2ecb4dde96779290c6
James Nguyen <nguyenvanviet1210@gmail.com>
2020-12-29 16:16:11 -0800

    Initial commit

---
+++ src/test/java/com/james/springenvironmentpractice/SpringenvironmentpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.springenvironmentpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringenvironmentpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
